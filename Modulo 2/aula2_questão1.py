25a523a018ea9bb18392fee069ed5a72d42d36b1
LeandroRodrigues117 <164257123+LeandroRodrigues117@users.noreply.github.com>
2024-07-20 07:53:11 -0300

    Add files via upload

---
+++ Modulo 2/aula2_questão1.py
@@ -0,0 +1,7 @@
+variavel1 = 5
+variavel2 = 2
+print (type(variavel1))
+print (type(variavel2))
+variavel3 = (variavel1/variavel2)
+print (variavel3)
+print (type(variavel3))
---
+++ Modulo 2/aula2_questão2.py
@@ -0,0 +1,12 @@
+texto = "O resultado é"
+valor1 = 10
+valor2 = 3.5
+soma_valores = (valor1+valor2)
+print (texto)
+print (type(texto))
+print (valor1)
+print (type(valor1))
+print (valor2)
+print (type(valor2))
+print (soma_valores)
+print (type(soma_valores))
